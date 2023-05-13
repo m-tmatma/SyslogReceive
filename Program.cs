417b92178537a4ffb2db70e95570cce530a56d11
Masaru Tsuchiyama <m.tmatma@gmail.com>
2023-05-13 22:31:57 +0900

    Implement Udp receive.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace SyslogReceive
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SyslogReceive
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var localPort = 514;
+            var localEP = new IPEndPoint(0, localPort);
+            using (UdpClient udp = new UdpClient(localEP))
+            {
+                while (true)
+                {
+                    IPEndPoint? remoteEP = null;
+                    byte[] data = udp.Receive(ref remoteEP);
+                    string message = System.Text.Encoding.UTF8.GetString(data);
+
+                    string address = remoteEP.Address.ToString();
+                    string filename = address + ".txt";
+                    var fs = File.Open(filename, FileMode.Append, FileAccess.Write, FileShare.Read);
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        Console.WriteLine($"{address} : {message}");
+                        sw.WriteLine($"{address} : {message}");
+                    }
+                }
+            }
         }
     }
 }
